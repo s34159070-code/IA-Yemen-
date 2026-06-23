c3ec78a44e4707e0cf1546cee323a0300e4a4a4a
s34159070-code <s34159070@gmail.com>
2026-06-23 23:54:17 +0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import "@/global.css";
+٦import "@/global.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
